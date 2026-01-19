e0170a8367763efd9df5cf8735f523b449ffeb4d
Александр Лезжов <alexander.lezzov@Noutbuk-Aleksandr.local>
2026-01-19 08:23:47 +0300

    comments tasks 1, 5

--- ga_test_task/task_5.sql
+++ ga_test_task/task_5.sql
@@ -1,9 +1,10 @@
 /*  Тестил на реальном клике, правильно считает интервал и стык между концом и началом года */
+/* Для максмальной корректности тоже можно наджоинить на календарь как задании 4  */
 
 SELECT DISTINCT
     toDate(timestamp) AS dt
     , COUNT(DISTINCT user_id) OVER (ORDER BY toRelativeDayNum(toDate(timestamp) )
-        RANGE BETWEEN 6 PRECEDING AND CURRENT ROW) AS wau
+            RANGE BETWEEN 6 PRECEDING AND CURRENT ROW) AS wau
 FROM dwh.site_activity
 WHERE toDate(timestamp)
 ORDER BY dt DESC
